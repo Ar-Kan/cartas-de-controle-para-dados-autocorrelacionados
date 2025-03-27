b278d9d7ecf6f282d3fedcdb8bacf9489de1cc50
Arquimedes Macedo <arqui13a@hotmail.com>
2025-03-27 00:56:08 -0300

    Fix bootstrap limits

--- coeficientes_bootstrap.R
+++ coeficientes_bootstrap.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(ggformula)
 library(dplyr)
 library(data.table)
+library(writexl)
 
 BETA_ARMA <- FALSE
 
@@ -62,6 +63,7 @@ fit <- function(yt, start) {
         yt,
         order = c(1, 0, 0),
         include.mean = FALSE,
+        method = "ML"
       )
     )
   }
@@ -77,8 +79,8 @@ fit <- function(yt, start) {
   )
 }
 
-seq_colagens <- c(1, 24, 49, 75, 99)
-phis <- c(-0.1, 0.1, 0.2, 0.3)
+seq_colagens <- c(1, 24, 49, 75, 99, 199)
+desvios_do_phi <- c(0, -0.1, 0.1, -0.3)
 tamanho_amostras_iniciais <- c(25, 50, 100, 200)
 
 df <- data.table(
@@ -87,11 +89,17 @@ df <- data.table(
   # Phi estimado para cada tamanho de colagem
   phi_estimado = numeric(),
   novo_phi = numeric(),
+  desvio = numeric(),
   # SEQ dos phis para cada tamanho de colagem
-  controle = logical()
+  fora_de_controle = logical(),
+  limite.inf = numeric(),
+  limite.sup = numeric()
 )
 
-TAMANHO <- 500
+tempo_inicial <- Sys.time()
+print(paste0("Começando simulações para ", ifelse(BETA_ARMA, "BARMA", "AR"), "... ", tempo_inicial))
+
+TAMANHO <- 300
 for (k in 1:TAMANHO) {
   for (n_inicial in tamanho_amostras_iniciais) {
     amostra_inicial <- sim(n = n_inicial, coefs = coeficientes())
@@ -100,27 +108,27 @@ for (k in 1:TAMANHO) {
       yt = amostra_inicial,
       start = list(alpha = 0.1, nu = 20, phi = 0.1)
     )
-    coef_inicial
     alpha.est <- ifelse(BETA_ARMA, coef_inicial$coef["alpha"][[1]], NA)
     nu.est <- ifelse(BETA_ARMA, coef_inicial$coef["nu"][[1]], NA)
     phi.est <- ifelse(BETA_ARMA, coef_inicial$coef["phi"][[1]], coef_inicial$coef[[1]])
     
     if (BETA_ARMA & phi.est > 0.6) {
+      print("Phi maior que 0.6, pulando...")
       next
     }
     
     print(paste0("Bootstrap (", k, ") - phi: ", phi.est, ", n: ", n_inicial))
     bootstrap <- list()
-    for (i in 1:1000) {
+    for (boots in 1:1000) {
       amostra <- sim(
-        n = 100,
+        n = n_inicial,
         coefs = coeficientes(alpha = alpha.est, nu = nu.est, phi = phi.est)
       )
       coef <- fit(
         yt = amostra,
         start = list(alpha = alpha.est, nu = nu.est, phi = phi.est)
       )
-      bootstrap[[i]] <- list(
+      bootstrap[[boots]] <- list(
         amostra = amostra,
         coef = list(
           alpha = ifelse(BETA_ARMA, coef$coef["alpha"][[1]], NA),
@@ -137,25 +145,25 @@ for (k in 1:TAMANHO) {
     )
     limite.sup <- quantile(
       unlist(lapply(bootstrap, function(x) x$coef$phi)),
-      probs = 0.95
+      probs = 0.975
     )
     
     
-    for (novo_phi in phis) {
+    for (desvio in desvios_do_phi) {
       # Próximas amostras
+      novo_phi <- phi.est + desvio
       proximas_amostras_100 <- sim(
-        n = n_inicial,
+        n = 200,
         coefs = coeficientes(alpha = alpha.est, nu = nu.est, phi = novo_phi)
       )
-      for (i in seq_colagens) {
-        proximas_amostras <- proximas_amostras_100[1:i]
-        if (i > n_inicial) {
-          next
-        } else if (i == n_inicial) {
+      for (sc in seq_colagens) {
+        proximas_amostras <- proximas_amostras_100[1:sc]
+        if (sc >= n_inicial) {
+          # sem colagem, apenas amostras novas
           novo_dataset <- proximas_amostras
         } else {
           novo_dataset <- c(
-            amostra_inicial[(i + 1):length(amostra_inicial)],
+            amostra_inicial[(sc + 1):length(amostra_inicial)],
             proximas_amostras
           )
         }
@@ -178,10 +186,13 @@ for (k in 1:TAMANHO) {
           df,
           data.table(
             tamanho_inicial = n_inicial,
-            tamanho_colagem = i,
+            tamanho_colagem = sc,
             phi_estimado = phi.nova.amostra,
             novo_phi = novo_phi,
-            controle = phi.nova.amostra < limite.inf | phi.nova.amostra > limite.sup
+            desvio = desvio,
+            fora_de_controle = (phi.nova.amostra < limite.inf) | (phi.nova.amostra > limite.sup),
+            limite.inf = limite.inf,
+            limite.sup = limite.sup
           )
         )
       }
@@ -189,27 +200,43 @@ for (k in 1:TAMANHO) {
   }
 }
 
+tempo_final <- Sys.time()
+print(paste0("Fim das simulações para ", ifelse(BETA_ARMA, "BARMA", "AR"), "... ", tempo_final))
+print(paste0("Tempo total: ", tempo_final - tempo_inicial))
+
 df_resumo <- df %>%
-  group_by(tamanho_inicial, tamanho_colagem, novo_phi) %>%
+  group_by(
+    tamanho_inicial,
+    tamanho_colagem,
+    desvio
+  ) %>%
   summarise(
-    controle = sum(controle),
+    controle = sum(fora_de_controle),
     total = n()
   ) %>%
   mutate(
     proporcao = controle / total
   )
 
-p <- ggplot(df_resumo, aes(x = tamanho_colagem, y = proporcao, color = as.factor(novo_phi))) +
+p <- ggplot(df_resumo, aes(x = tamanho_colagem, y = proporcao, color = as.factor(desvio))) +
+  geom_hline(yintercept = 0.05, linetype = "dashed") +
   geom_point() +
   geom_line() +
   labs(
     x = "Tamanho da colagem",
     y = "Proporção de controle",
-    color = "Novo valor de Phi"
+    color = "Desvio do Phi",
+    title = "Proporção de controle fora dos limites"
   ) +
   theme_minimal() +
   facet_wrap(~tamanho_inicial)
 
-ggsave("controle_ar.png", p, width = 10, height = 6, units = "in", dpi = 300)
+if (BETA_ARMA) {
+  ggsave("controle_bar.png", p, width = 10, height = 6, units = "in", dpi = 300)
+  write_xlsx(df, "df_bar.xlsx")
+} else {
+  ggsave("controle_ar.png", p, width = 10, height = 6, units = "in", dpi = 300)
+  write_xlsx(df, "df_ar.xlsx")
+}
 
 p
