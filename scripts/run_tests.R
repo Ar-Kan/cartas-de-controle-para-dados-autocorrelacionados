405054032a4fed0107fc63a846c9cc749100161a
Ar-Kan <arqui13a@hotmail.com>
2026-04-04 01:25:32 -0300

    Atualiza execução de testes

--- scripts/run_tests.R
+++ scripts/run_tests.R
@@ -1,9 +1,12 @@
-options(testthat.default_reporter = "progress")
-options(encoding = "UTF-8")
+options(
+  testthat.default_reporter = "progress",
+  encoding = "UTF-8"
+)
 
 # Evita paralelismo dentro dos testes
 if (requireNamespace("future", quietly = TRUE)) {
   future::plan(future::sequential)
 }
 
-testthat::test_dir("tests/testthat", reporter = "progress")
+devtools::load_all()
+devtools::test()
--- tests/testthat/helper-load_code.R
+++
@@ -1,3 +0,0 @@
-source(testthat::test_path("..", "..", "R", "paralelizacao.R"), encoding = "UTF-8")
-source(testthat::test_path("..", "..", "R", "utils.R"), encoding = "UTF-8")
-source(testthat::test_path("..", "..", "R", "arma.R"), encoding = "UTF-8")
